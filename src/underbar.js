d6c55300f60bd89c926efccabc2ee3148a2440f6
Jesse Beard <jjbeard@protonmail.com>
2020-03-24 16:59:05 -0700

    we finished both first and last

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,8 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+  //returns the value
+  return val;
   };
 
   /**
@@ -25,7 +27,7 @@
    * in your work on the following functions. Whenever you see a portion of the
    * assignment pre-completed, be sure to read and understand it fully before
    * you proceed. Skipping this step will lead to considerably more difficulty
-   * implementing the sections you are responsible for.
+   * implementing the sections you are responsible for.`
    */
 
   // Return an array of the first n elements of an array. If n is undefined,
@@ -37,6 +39,11 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+  if ( n > array.length){
+    return array;
+  } else if (n === undefined){
+      return array[array.length-1];
+    } else {return array.slice (array.length - n, array.length)}
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -107,19 +114,19 @@
   // Reduces an array or object to a single value by repetitively calling
   // iterator(accumulator, item) for each item. accumulator should be
   // the return value of the previous iterator call.
-  //  
+  //
   // You can pass in a starting value for the accumulator as the third argument
   // to reduce. If no starting value is passed, the first element is used as
   // the accumulator, and is never passed to the iterator. In other words, in
   // the case where a starting value is not passed, the iterator is not invoked
   // until the second element, with the first element as its second argument.
-  //  
+  //
   // Example:
   //   var numbers = [1,2,3];
   //   var sum = _.reduce(numbers, function(total, number){
   //     return total + number;
   //   }, 0); // should be 6
-  //  
+  //
   //   var identity = _.reduce([5], function(total, number){
   //     return total + number * number;
   //   }); // should be 5, regardless of the iterator function passed in
